10a145b8aa9d0542f9a4adfdb7dc4567e847ed1b
Timon <thebestgamer.mrt@outlook.com>
2026-02-07 21:18:59 +0100

    Updated navigation with new backend structure and new links

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -58,6 +58,17 @@
         goto(newUrl, { keepfocus: true, noScroll: true });
     }
 
+    let submenuOpen = null;
+
+    function openSubmenu(name) {
+        submenuOpen = name;
+    }
+
+    function closeNavigation() {
+        navigationOpen.set(false);
+        submenuOpen = null;
+    }
+
 </script>
 
 {#if lang === "en"}
@@ -198,7 +209,7 @@
 
     <nav class="navigation" class:navigation-open={$navigationOpen}>
 
-        <ul class="navigation-menu">
+        <ul class="navigation-menu" class:hidden={submenuOpen !== null}>
             <li class="navigation-section">
                 <div class="language-switcher" class:de={lang === 'de'} class:en={lang === 'en'}>
                     <div class="language-switcher-slider"></div>
@@ -229,75 +240,151 @@
             </li>
 
             <li class="navigation-element">
-                <div class="navigation-table">
-                    <div class="navigation-table-element">
-                        <IconHome class="navigation-icon" />
+                <a class="navigation-link" href="/de/" on:click={closeNavigation}>
+                    <div class="navigation-table">
+                        <div class="navigation-table-element">
+                            <IconHome class="navigation-icon" />
+                        </div>
+                        <div class="navigation-table-element">
+                            <span>Startseite</span>
+                        </div>
                     </div>
-                    <div class="navigation-table-element">
-                        <a class="navigation-link" href="/de/">Startseite</a>
-                    </div>
-                </div>
+                </a>
             </li>
 
             <li class="navigation-element navigation-element-active">
-                <input class="navigation-input" type="checkbox" id="navigation-input-1" aria-hidden="true">
-                <label class="navigation-label" for="navigation-input-1">
+                <button class="navigation-label" on:click={() => openSubmenu("content")}>
                     <div class="navigation-table">
                         <div class="navigation-table-element">
                             <IconApps class="navigation-icon" />
                         </div>
                         <div class="navigation-table-element">
-                            <p class="navigation-link">Inhalte ansehen</p>
+                            <span class="navigation-link">Inhalte</span>
                         </div>
                         <div class="navigation-table-element">
                             <IconChevronRight class="navigation-icon" />
                         </div> 
                     </div>
-                </label>
+                </button>
+            </li>
 
-                <ul class="navigation-submenu">
-                    <div class="navigation-section">                    
-                        <li class="navigation-element">
-                            <label class="navigation-label" for="navigation-input-1">
-                                <div class="navigation-table">
-                                    <div class="navigation-table-element">
-                                        <IconChevronLeft class="navigation-icon" />
-                                    </div>
-                                    <div class="navigation-table-element">
-                                        <p class="navigation-link">Zurück</p>
-                                    </div>
-                                </div>
-                            </label>
-                        </li>
-
-                        <span class="navigation-line"></span>
-                    </div>
-                    
-                    <li class="navigation-element navigation-element-active"> 
-                        <div class="navigation-table">
-                            <div class="navigation-table-element">
-                                <IconApps class="navigation-icon" />
-                            </div>
-                            <div class="navigation-table-element">
-                                <a class="navigation-link" href="/de/content/">Alle Inhalte</a>
-                            </div>
+            <li class="navigation-element">
+                <button class="navigation-label" on:click={() => openSubmenu("create")}>
+                    <div class="navigation-table">
+                        <div class="navigation-table-element">
+                            <IconApps class="navigation-icon" />
                         </div>
-                    </li>
+                        <div class="navigation-table-element">
+                            <span class="navigation-link">Erstellen</span>
+                        </div>
+                        <div class="navigation-table-element">
+                            <IconChevronRight class="navigation-icon" />
+                        </div> 
+                    </div>
+                </button>
+            </li>
+        </ul>
 
-                    <li class="navigation-element">
-                        <input class="navigation-input" type="checkbox" id="navigation-input-1-test" aria-hidden="true">
-                        <label class="navigation-label" for="navigation-input-1-test">
-                            <div class="navigation-table">
-                                <div class="navigation-table-element">
-                                    <IconCode class="navigation-icon" />
-                                </div>
-                                <div class="navigation-table-element">
-                                    <a class="navigation-link" href="/de/content/">Eigene Befehle</a>
-                                </div>                        
-                            </div>
-                        </label>           
-                    </li>
-                </ul>
+        <ul class="navigation-submenu" class:active={submenuOpen === "content"}>
+            <li class="navigation-element">
+                <button class="navigation-label" on:click={() => submenuOpen = null}>
+                    <div class="navigation-table">
+                        <div class="navigation-table-element">
+                            <IconChevronLeft class="navigation-icon" />
+                        </div>
+                        <div class="navigation-table-element">
+                            <span class="navigation-link">Zurück</span>
+                        </div>
+                    </div>
+                </button>
+            </li>
+
+            <li class="navigation-section">
+                <span class="navigation-line"></span>
+            </li>
+            
+            <li class="navigation-element navigation-element-active">
+                <a class="navigation-link" href="/de/content/" on:click={closeNavigation}>
+                    <div class="navigation-table">
+                        <div class="navigation-table-element">
+                            <IconApps class="navigation-icon" />
+                        </div>
+                        <div class="navigation-table-element">
+                            <span>Alle Inhalte</span>
+                        </div>
+                    </div>
+                </a>
+            </li>
+
+            <li class="navigation-element">
+                <a class="navigation-link" href="/de/content/" on:click={closeNavigation}>
+                    <div class="navigation-table">
+                        <div class="navigation-table-element">
+                            <IconCode class="navigation-icon" />
+                        </div>
+                        <div class="navigation-table-element">
+                            <span>Eigene Befehle</span>
+                        </div>
+                    </div>
+                </a>
+            </li>
+
+            <li class="navigation-element">
+                <a class="navigation-link" href="/de/content/" on:click={closeNavigation}>
+                    <div class="navigation-table">
+                        <div class="navigation-table-element">
+                            <IconApps class="navigation-icon" />
+                        </div>
+                        <div class="navigation-table-element">
+                            <span>Nachrichten-Vorlagen</span>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        </ul>
+
+        <ul class="navigation-submenu" class:active={submenuOpen === "create"}>
+            <li class="navigation-element">
+                <button class="navigation-label" on:click={() => submenuOpen = null}>
+                    <div class="navigation-table">
+                        <div class="navigation-table-element">
+                            <IconChevronLeft class="navigation-icon" />
+                        </div>
+                        <div class="navigation-table-element">
+                            <span class="navigation-link">Zurück</span>
+                        </div>
+                    </div>
+                </button>
+            </li>
+
+            <li class="navigation-section">
+                <span class="navigation-line"></span>
+            </li>
+
+            <li class="navigation-element">
+                <a class="navigation-link" href="/de/create/" on:click={closeNavigation}>
+                    <div class="navigation-table">
+                        <div class="navigation-table-element">
+                            <IconApps class="navigation-icon" />
+                        </div>
+                        <div class="navigation-table-element">
+                            <span>Link 1</span>
+                        </div>
+                    </div>
+                </a>
+            </li>
+
+            <li class="navigation-element">
+                <a class="navigation-link" href="/de/create/" on:click={closeNavigation}>
+                    <div class="navigation-table">
+                        <div class="navigation-table-element">
+                            <IconApps class="navigation-icon" />
+                        </div>
+                        <div class="navigation-table-element">
+                            <span>Link 2</span>
+                        </div>
+                    </div>
+                </a>
             </li>
         </ul>
 
